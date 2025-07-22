4d6d5d1b7dfb7253dd8aa07088426d8352897c93
AndrewIsDev <ondraminka@gmail.com>
2025-07-22 22:49:19 +0200

    Tested + fixed control.lua. Now prints ticks in game

--- resource-outpost-generator/control.lua
+++ resource-outpost-generator/control.lua
@@ -1,10 +1,13 @@
-function on_init()
+script.on_init(function()
 	print('Hello, world! Greets init')
 	game.print('Hello, world! Greets init')
-end
+end)
 
-function on_load()
+script.on_load(function()
 	print('Hello, world! Greets load')
-	game.print('Hello, world! Greets load')
-end
+	--game.print('Hello, world! Greets load')
+end)
 
+script.on_event(defines.events.on_tick, function(event)
+	game.print(event.tick)
+end)
