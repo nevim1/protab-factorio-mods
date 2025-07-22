233a1a40e22b23909d3e6bc33cd10b04f066c27f
nevim <ondrej.lupisek@gmail.com>
2025-07-22 22:20:51 +0200

    added prints

--- resource-outpost-generator/control.lua
+++ resource-outpost-generator/control.lua
@@ -1,8 +1,10 @@
 function on_init()
+	print('Hello, world! Greets init')
 	game.print('Hello, world! Greets init')
 end
 
 function on_load()
+	print('Hello, world! Greets load')
 	game.print('Hello, world! Greets load')
 end
 
