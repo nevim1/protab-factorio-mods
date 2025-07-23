6a8d4b7b424649f45031331b41b27bfc2cb88121
AndrewIsDev <ondraminka@gmail.com>
2025-07-23 13:55:16 +0200

    Made the tool appear on cursor. Also added the selection box.

--- resource-outpost-generator/control.lua
+++ resource-outpost-generator/control.lua
@@ -17,9 +17,11 @@ end)
 script.on_event(defines.events.on_lua_shortcut, function(event)
 	if event.prototype_name == 'print-hello-to-console' then
 		game.print('hello from our shortcut')
-		for i,j in pairs(event) do
+		for i, j in pairs(event) do
 			print(i, j)
-			game.print(i .. ', ' ..  j)
+			game.print(i .. ', ' .. j)
 		end
+		local player = game.players[event.player_index]
+		player.cursor_stack.set_stack { name = "gen-tool", count = 1 }
 	end
 end)
--- resource-outpost-generator/data.lua
+++ resource-outpost-generator/data.lua
@@ -7,14 +7,43 @@ local outpostGeneratorPrototype = {
 	icon_size = 56,
 	small_icon_size = 24
 }
-data:extend({outpostGeneratorPrototype})
+data:extend({ outpostGeneratorPrototype })
 
 local shortcutProto = data.raw["shortcut"]
 print('------------- shortcut dump -------------')
 for i, j in pairs(shortcutProto) do
 	print(i)
-	for k,l in pairs(j) do
-		print('+', k,l)
+	for k, l in pairs(j) do
+		print('+', k, l)
 	end
 end
 print('------------- shortcut dump -------------')
+
+
+local outpostGeneratorToolPrototype = {
+	type = "selection-tool",
+	name = "gen-tool",
+	--action = "lua",
+	icon = "__base__/graphics/icons/shapes/shape-diagonal.png",
+	icon_size = 64,
+	flags = { "only-in-cursor", "not-stackable", "spawnable" },
+	auto_recycle = "false",
+	subgroup = "tool",
+	hidden = true,
+	stack_size = 1,
+	order = "c[automated-construction]-x",
+	draw_label_for_cursor_render = true,
+	select = {
+		border_color = { 0, 1, 0 },
+		mode = { "blueprint", "avoid-rolling-stock", "avoid-vehicle" },
+		cursor_box_type = "copy",
+		started_sound = { filename = "__core__/sound/blueprint-select.ogg" },
+		ended_sound = { filename = "__core__/sound/blueprint-create.ogg" }
+	},
+	alt_select = {
+		border_color = {0, 0, 1},
+		mode = {"blueprint"},
+		cursor_box_type = "copy"
+	}
+}
+data:extend({ outpostGeneratorToolPrototype })
