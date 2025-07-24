a9df1e9c6c26165af70ff88e9af8e2a1b892d546
AndrewIsDev <ondraminka@gmail.com>
2025-07-24 22:21:37 +0200

    Minor changes to file

--- resource-outpost-generator/control.lua
+++ resource-outpost-generator/control.lua
@@ -7,18 +7,6 @@ script.on_load(function()
 	print('Hello, world! Greets load')
 end)
 
-script.on_event(defines.events.on_lua_shortcut, function(event)
-	if event.prototype_name == 'gen-shortcut' then
-		game.print('hello from our shortcut')
-		for i, j in pairs(event) do
-			print(i, j)
-			game.print(i .. ': ' .. j)
-		end
-		local player = game.players[event.player_index]
-		player.cursor_stack.set_stack { name = "gen-tool", count = 1 }
-	end
-end)
-
 -- startingPositions :: array[MapPosition], endingPositions :: array[MapPosition]
 -- returns positions on path: array[MapPositions]
 --[[
@@ -40,7 +28,7 @@ local function bfs(path, queue, goals)
 		local currentPosition = queue[0].position
 		local visited = queue[0].visited
 		queue:remove(0)
-		if surface.can_place_entity({name = 'pipe', position = currentPosition}) then
+		if surface.can_place_entity({ name = 'pipe', position = currentPosition }) then
 			if visited[MPtoStr(currentPosition)] ~= true then
 				visited[MPtoStr(currentPosition)] = true
 			end
@@ -48,26 +36,28 @@ local function bfs(path, queue, goals)
 	end
 end
 
+--SHORTCUT EVENTS
 
-script.on_event("CustomRightClick", function(event)
-	local x = event.cursor_position.x
-	local y = event.cursor_position.y
-	print("Right click registered at: ", "x: " .. x, "y: " .. y)
-	game.print("Right click registered")
+script.on_event(defines.events.on_lua_shortcut, function(event)
+	if event.prototype_name == 'gen-shortcut' then
+		game.print('hello from our shortcut')
+		for i, j in pairs(event) do
+			print(i, j)
+			game.print(i .. ': ' .. j)
+		end
+		local player = game.players[event.player_index]
+		player.cursor_stack.set_stack { name = "gen-tool", count = 1 }
+	end
 end)
 
 
+
+
+--SELECTION EVENTS
+
 script.on_event(defines.events.on_player_selected_area, function(event)
 	if event.item == "gen-tool" then
-		--print("serpent: ", serpent.block(event.entities))
-		--[[	local resources = {
-			["coal"] = true,
-			["stone"] = true,
-			["copper-ore"] = true,
-			["crude-oil"] = true
-		}]]
 		for i, e in ipairs(event.entities) do
-			--print(i, e.name, e.gps_tag)
 			if e.name == "crude-oil" then
 				print("build crude-oil outpost at:")
 				local x = e.position.x
@@ -76,16 +66,15 @@ script.on_event(defines.events.on_player_selected_area, function(event)
 				game.print("build crude-oil outpost at:")
 				game.print("x: " .. x .. " y: " .. y)
 				print("crude_oil bounding_box: ", serpent.block(e.bounding_box))
-				--game.print("bounding_box: ".. e.bounding_box)
 
-				pumpjack = {
+				local pumpjack = {
 					name = 'entity-ghost',
 					position = e.position,
 					force = game.players[event.player_index].force,
 					inner_name = 'pumpjack',
 					direction = defines.direction.south
 				}
-				setting_val = settings.global["AutoBuildOutpost"].value
+				local setting_val = settings.global["AutoBuildOutpost"].value
 				print("setting_cal:", setting_val)
 				if setting_val == true then
 					pumpjack.name = 'pumpjack'
@@ -116,4 +105,14 @@ script.on_event(defines.events.on_player_alt_selected_area, function(event)
 			print(j)
 		end
 	end
+end)
+
+
+--OTHER EVENTS
+
+script.on_event("CustomRightClick", function(event)
+	local x = event.cursor_position.x
+	local y = event.cursor_position.y
+	print("Right click registered at: ", "x: " .. x, "y: " .. y)
+	game.print("Right click registered")
 end)
